594f10ae1d5a85f6e375df136610c316a63715e4
Raoul Harel <raoulhaa@gmail.com>
2015-09-09 21:02:14 +0200

    added seek behavior and visual test suite

---
+++ src/behaviors/Seek.js
@@ -0,0 +1,44 @@
+/**
+ * @author Raoul Harel
+ * @license The MIT license (LICENSE.txt)
+ * @copyright 2015 Raoul Harel
+ * @url https://github.com/rharel/js-steering-behaviors
+ */
+
+
+function Seek(target, desired_speed, flee) {
+
+  this._target = target.clone();
+  this._desired_speed = desired_speed;
+  this._flee = flee || false;
+}
+
+
+Seek.prototype = {
+
+  constructor: Seek,
+
+  drive: function(character) {
+
+    var desired_velocity =
+      this._target
+        .sub(character.position)
+        .unit_()
+        .scale_(this._desired_speed);
+
+    if (this._flee) { desired_velocity.scale_(-1); }
+
+    return desired_velocity.sub(character.velocity);
+  },
+
+  get target() { return this._target; },
+
+  get desired_speed() { return this._desired_speed; },
+  set desired_speed(value) { this._desired_speed = +value; },
+
+  get flee() { return this._flee; },
+  set flee(value) { this._flee = !!value; }
+};
+
+
+module.exports = Seek;
